7d24cb8f09a8cd01c8962b8bb891aee652936f33
Ulf Lilleengen <lulf@redhat.com>
2019-05-18 15:48:03 +0200

    Add db support

--- cmd/event-sink/main.go
+++ cmd/event-sink/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"os"
 	"pack.ag/amqp"
 )
 
 func main() {
+
+	dbfile := "./test.db"
+	os.Remove(dbfile)
+
+	db, err := sql.Open("sqlite3", dbfile)
+	if err != nil {
+		log.Fatal("Opening Database:", err)
+	}
+	defer db.Close()
+
 	client, err := amqp.Dial("amqps://localhost:5671", amqp.ConnSASLPlain("test", "test"))
 	if err != nil {
 		log.Fatal("Dialing AMQP server:", err)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lulf/teig-event-sink
 go 1.12
 
 require (
+	github.com/mattn/go-sqlite3 v1.10.0
 	pack.ag/amqp v0.11.0
 	rsc.io/quote v1.5.2
 )
