f93973c9244d9d51642b1b39858daf91c0ce3d63
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-08-05 16:44:01 +0200

    Fix stream

--- src/main/java/cloud/teig/event/sink/hono/HonoConsumer.java
+++ src/main/java/cloud/teig/event/sink/hono/HonoConsumer.java
@@ -9,6 +9,7 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.Map;
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class HonoConsumer {
@@ -17,21 +18,23 @@ public class HonoConsumer {
     Emitter<Event> eventEmitter;
 
     @Incoming("telemetry")
-    public void processTelemetry(AmqpMessage<Map> message) {
-        handleMessage(message);
+    public CompletionStage<Void> processTelemetry(AmqpMessage<Map> message) {
+        return handleMessage(message);
     }
 
     @Incoming("event")
-    public void processEvent(AmqpMessage<Map> message) {
-        handleMessage(message);
+    public CompletionStage<Void> processEvent(AmqpMessage<Map> message) {
+        return handleMessage(message);
     }
 
-    private void handleMessage(AmqpMessage<Map> message) {
+    private CompletionStage<Void> handleMessage(AmqpMessage<Map> message) {
         String deviceId = message.getApplicationProperties().getString("device_id");
         long creationTime = message.getCreationTime();
         Event event = new Event(deviceId, creationTime, message.getPayload());
         System.out.println("Produced event: " + event);
-        eventEmitter.send(event).thenApply(f -> message.ack())
-                .exceptionally(message::nack);
+        return eventEmitter.send(event).thenAccept(f -> message.ack()).exceptionally(f -> {
+            message.nack(f);
+            return null;
+        });
     }
 }
